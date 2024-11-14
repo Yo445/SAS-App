3a9cbceaaeb467ce89d83db236e86e91af47d8fa
Yo445 <ya10894@gmail.com>
2024-11-14 15:53:14 +0200

    make edits

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import './index.css'
-import Navbar from './components/shared/Navbar';
-import Landing from './pages/landing/Landing';
-import Footer from './components/shared/Footer';
-import ListofSevices from './pages/landing/ListofSevices';
-import Login from './pages/Auth/Login';
-import Signup from './pages/Auth/Signup';
-import Contacts from './pages/landing/Contacts';
+import "./index.css";
+import Navbar from "./components/shared/Navbar";
+import Landing from "./pages/landing/Landing";
+import Footer from "./components/shared/Footer";
+import ListofSevices from "./pages/landing/ListofSevices";
+import Login from "./pages/Auth/Login";
+import Signup from "./pages/Auth/Signup";
+import Contacts from "./pages/landing/Contacts";
+import { Outlet } from "react-router-dom";
+import NotFound from "./components/shared/NotFound ";
 
 function App() {
-
   return (
     <>
-        {/* <Navbar />
-        <Landing />
-        <ListofSevices />
-        <Footer /> */}
-        <Login />
+      <Navbar />
+      <NotFound />
+      <Footer />
+      {/* <Login /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
